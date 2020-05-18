0d2cfe3f911a030342f5ccb92f167d09d2d405a8
Petr Panteleyev <petr@panteleyev.org>
2020-05-18 22:32:22 +0300

    OFX Parser 0.1.1
    * StatementTransaction converted to record

--- README.md
+++ README.md
@@ -3,22 +3,11 @@
 This library provides JavaFX helper classes.
 
 [![BSD-2 license](https://img.shields.io/badge/License-BSD--2-informational.svg)](LICENSE)
-[![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.panteleyev/ofx/badge.svg)](https://maven-badges.herokuapp.com/maven-central/org.panteleyev/ofx-parser/)
-[![Javadocs](http://www.javadoc.io/badge/org.panteleyev/java-fx-helpers.svg)](http://www.javadoc.io/doc/org.panteleyev/ofx-parser)
-
-
-## Artifact
-
-```xml
-<artifact>
-    <groupId>org.panteleyev</groupId>
-    <artifactId>ofx-parser</artifactId>
-    <version>0.1.0</version>
-</artifact>
-```
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.panteleyev/ofx-parser/badge.svg)](https://maven-badges.herokuapp.com/maven-central/org.panteleyev/ofx-parser/)
+[![Javadocs](http://www.javadoc.io/badge/org.panteleyev/ofx-parser.svg)](http://www.javadoc.io/doc/org.panteleyev/ofx-parser)
 
 ## JDK Compatibility
 
 | Version | JDK |
 |---|---|
-|0.1.0|14|
+|0.1.0, 0.1.1|14|
--- src/main/java/org/panteleyev/ofx/StatementTransaction.java
+++ src/main/java/org/panteleyev/ofx/StatementTransaction.java
@@ -7,85 +7,16 @@ package org.panteleyev.ofx;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-public class StatementTransaction {
-    private final TransactionEnum type;
-    private final LocalDateTime datePosted;
-    private final LocalDateTime dateUser;
-    private final LocalDateTime dateAvailable;
-    private final BigDecimal amount;
-    private final String name;
-    private final String memo;
-    private final String checkNum;
-
-    StatementTransaction(TransactionEnum type, LocalDateTime datePosted, LocalDateTime dateUser,
-                                LocalDateTime dateAvailable, BigDecimal amount, String name, String memo,
-                                String checkNum) {
-        this.type = type;
-        this.datePosted = datePosted;
-        this.dateUser = dateUser;
-        this.dateAvailable = dateAvailable;
-        this.amount = amount;
-        this.name = name;
-        this.memo = memo;
-        this.checkNum = checkNum;
-    }
-
-    public LocalDateTime getDatePosted() {
-        return datePosted;
-    }
-
-    public LocalDateTime getDateUser() {
-        return dateUser;
-    }
-
-    public LocalDateTime getDateAvailable() {
-        return dateAvailable;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getMemo() {
-        return memo;
-    }
-
-    public String getCheckNum() {
-        return checkNum;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-
-        if (!(obj instanceof StatementTransaction)) {
-            return false;
-        }
-
-        StatementTransaction that = (StatementTransaction)obj;
-
-        return Objects.equals(this.type, that.type)
-            && Objects.equals(this.amount, that.amount)
-            && Objects.equals(this.checkNum, that.checkNum)
-            && Objects.equals(this.dateAvailable, that.dateAvailable)
-            && Objects.equals(this.datePosted, that.datePosted)
-            && Objects.equals(this.dateUser, that.dateUser)
-            && Objects.equals(this.name, that.name)
-            && Objects.equals(this.memo, that.memo);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(type, amount, checkNum, dateAvailable, datePosted, dateUser, name, memo);
-    }
+public record StatementTransaction (
+    TransactionEnum type,
+    LocalDateTime datePosted,
+    LocalDateTime dateUser,
+    LocalDateTime dateAvailable,
+    BigDecimal amount,
+    String name,
+    String memo,
+    String checkNum) {
 
     public static class Builder {
         private TransactionEnum type;
